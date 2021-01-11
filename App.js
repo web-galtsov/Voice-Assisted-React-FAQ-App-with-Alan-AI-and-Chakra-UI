ed5958464a14d719987916c1fb9cc9c038ece49a
Oxana <oksana.galtsov@gmail.com>
2021-01-11 19:11:10 +0200

    add Navbar

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { ChakraProvider, theme } from '@chakra-ui/react';
+import { Navbar } from "./src/Navbar";
+
+export const App = () => {
+    return (
+        <ChakraProvider theme={theme}>
+           <Navbar/>
+        </ChakraProvider>
+    )
+}
